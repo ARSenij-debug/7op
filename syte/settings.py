135a7def36de12edb8c519ada31fab85e0d9310e
Арсений <arsenator337@gmail.com>
2020-09-22 22:39:22 +0400

    !!!

--- syte/settings.py
+++ syte/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'a!xmcqk8sq!xp03-g$kh-gy3^_rk*=l8+m-n^0km0530@lea@k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.семь-мнений.рф', '127.0.0.1']
+ALLOWED_HOSTS = ['www.семь-мнений.рф', '127.0.0.1', 'https://shielded-sea-91865.herokuapp.com']
 
 
 # Application definition
