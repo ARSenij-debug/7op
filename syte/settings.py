8744fed5228ea897b27758f9cb304451c6752453
Арсений <arsenator337@gmail.com>
2020-09-23 19:11:29 +0400

    \_/

--- syte/settings.py
+++ syte/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = '/static/'
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Configure Django App for Heroku.
+
+'''# Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals())'''
